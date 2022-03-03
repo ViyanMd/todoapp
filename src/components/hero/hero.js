a59d37badfee8bc52d01c5b5a0b31a6bbf1a926d
Viyan Md <home@192.168.1.4>
2022-03-04 00:04:17 +0300

    interim commit

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -7,19 +7,50 @@ const api = {
     base: "http://api.openweathermap.org/data/2.5/weather?"
 }
 
+
 const Hero = () => {
     const [weather, setWeather] = useState({})
+    const [query, setQuery] = useState(' ');
+    const [date, setDate] = useState(new DateObject());
+    const [response, setResponse] = useState();
 
-    function getWeather() {
-        fetch(`${api.base}q=Москва&units=metric&apikey=${api.key}`)
-        .then(res => res.json())
-        .then(data => setWeather(data.main));
+    
+    setInterval(() => {
+        setDate(new DateObject())
+    }, 60000)
+
+    function handleQuery(e) {
+        setQuery(e.target.value);
+        
     }
+
+    async function getWeather(e) {
+        e.preventDefault();
+
+        let response = await fetch(`${api.base}q=${query}&units=metric&apikey=${api.key}`);
+        setResponse(response.status);
+        let data =  await response.json();
+        setWeather(data.main);
+    }
+
+    console.log(date);
     return (
+        <div className="hero">
+        <form className="query__form" onSubmit={(e) => getWeather(e)}>
+            <input type="text"  onChange={(e) =>handleQuery(e)}></input>
+            {/* <button type="button" onClick={getWeather}></button> */}
+        </form>
         <div className="hero__section">
-            <h1>{Math.floor(weather.temp)}&#176;</h1>
-            <h3>Feels like: {Math.floor(weather.feels_like)}&#176;</h3>
-            <button onClick={getWeather}>Get Weather</button>
+            <div className="hero__section_date">{date.format("dddd").toUpperCase()}</div>
+            <div className={`hero__weather ${weather.temp ? 'active' : ""}`}>
+                <span className="hero__weather_temp">{Math.floor(weather.temp) ? Math.floor(weather.temp) : " "}&#176;</span>
+                <div className="hero__weather_extra">
+                    <span className="hero__weather_max">Max: {Math.floor(weather.temp_max)}&#176;</span>
+                    <span className="hero__weather_min">Min: {Math.floor(weather.temp_min)}&#176;</span>
+                    <span className="hero__weather_hum">Hum: {Math.floor(weather.humidity)}</span>
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -5,6 +5,28 @@ const ToDo = () => {
     const [input, setInput] = useState(" ");
     const [todos, setTodos] = useState([]);
 
+    function handleComplete(id) {
+        let list = [...todos];
+        let updatedList = list.map(item => {
+            if(item.id === id) {
+                return {...item, completed: !item.completed}; 
+            }
+            return item;
+        })
+        
+        setTodos(updatedList);
+    }
+
+    function removeTodo(e) {
+        let list = [...todos];
+        list.splice(list.indexOf(e.target.value), 1);
+        setTodos(list);
+    }
+
+    function filterTodos () {
+
+    }
+
     function handleInput (e) {
         setInput(e.target.value);
     }
@@ -27,18 +49,26 @@ const ToDo = () => {
     return (
         <div className="todo__section">
             <div className="todo__filter">
-                    <button className="btn__filter">All</button>
+                    <button className="btn__filter" onClick={filterTodos}>All</button>
                     <button className="btn__filter">In Process</button>
                     <button className="btn__filter">Completed</button>  
             </div>
             <form className="todo__form">
-                <input type="text" placeholder="What's your plan for today ?" onChange={handleInput} value={input}></input>
-                <button type="button" onClick={addItem}>Submit</button>
+                <input type="text" value={input} placeholder="What's your plan for today ?" onChange={handleInput}></input>
+                <button type="button" onClick={addItem}>Add</button>
             </form>
             <div className="todo__list">
                 <ul>
                     {todos.map(item => {
-                        return <li key={item.id}>{item.value}</li>
+                        return (
+                        <li className='todo__item' key={item.id}>
+                            <p>{item.value}</p>
+                            <div className="todo__action">
+                                <button className="todo__action_completed" onClick={() => handleComplete(item.id)}>Completed</button>
+                                <button className="todo__action_remove" onClick={removeTodo}>Remove</button>
+                            </div>
+                        </li> 
+                        );
                     })}
                 </ul>
             </div>
